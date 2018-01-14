60557724449e3d267ac892b3bed2fe5d4af2e2b3
Francesco <fraxdd@gmail.com>
2018-01-14 22:08:07 +0100

    fix display

--- primo_test/config.lua
+++ primo_test/config.lua
@@ -1,21 +1,19 @@
+local aspectRatio = display.pixelHeight / display.pixelWidth
 application = {
-	content = {
-		 --width = 800,
-		-- height = 1280, 
-		-- scale = "LetterBox",
-		fps = 60,
-		
-		--[[
-        imageSuffix = {
-		    ["@2x"] = 2,
-		}
-		--]]
-	},
+   content = {
+      width = aspectRatio > 1.5 and 1080 or math.ceil( 1920 / aspectRatio ),
+      height = aspectRatio < 1.5 and 1920 or math.ceil( 1080 * aspectRatio ),
+      scale = "LetterBox",
+      fps = 60,
+
+      imageSuffix = {
+         ["@2x"] = 1.8,
+      },
+   },
 	LevelDirectorSettings = 
 	{
 		imagesSubFolder = "images",
 		levelsSubFolder = "levels",
 	}
- 
+	
 }
-
